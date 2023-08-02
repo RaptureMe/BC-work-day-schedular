bf77be5267cce301a6f70d20b16fbc41fdf5a7af
RaptureMe <arosu4u@gmail.com>
2023-08-02 12:35:36 -0700

    Added hour checker and local storage for the text save

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,54 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// This is jQuery method to ensure DOM is fully loaded before running
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // This code is to bring in the current date through DayJS in proper format
+  var currentDay = dayjs().format("MMMM D, YYYY");
+  var currentDayElement = document.getElementById("currentDay");
+  currentDayElement.textContent = currentDay;
+
+  // you can add -1 for ex. after hour() to subtract the time back.
+  var currentHour = dayjs().hour();
+  console.log(currentHour);
+  $(".time-block").each(function () {
+    // Get the current ID for the time-block
+    var timeBlockId = $(this).attr("id");
+    // Check if there is localstorage for this ID
+    var storedVal = localStorage.getItem(timeBlockId);
+    var txtArea = $(this).find("textarea");
+    txtArea.val(storedVal);
+    // need to find the text area then append to that id text area
+    // Get the time from the data-time attribute
+
+    console.log(storedVal)
+    var blockHour = parseInt($(this).attr("data-time"));
+
+    // Check if we've moved past this time
+    // this cycles through the times through Datatime on the HTML to see if its past, present or future
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+  function buttonClick(event) {
+    // TODO: Create a variable that holds the clicked jquery button
+    // This holds the time block info
+    var clickedButton = $(event.target);
+
+    var timeBlock = clickedButton.parents(".time-block");
+    var timeBlockId = timeBlock.attr("id");
+
+    var txtVal = timeBlock.find(".description").val();
+    console.log(txtVal)
+
+    localStorage.setItem(timeBlockId,txtVal)
+    // TODO: Save the value of the text area into local storage
+  }
+
+  $(".saveBtn").on("click", buttonClick);
+  
 });
